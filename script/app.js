58cb57250892d02d72a1379c23d2a5c3f78d2617
foysalkazi11 <foysalkazi11@gmail.com>
2020-05-16 15:47:00 +0600

    remove function

--- script/app.js
+++ script/app.js
@@ -3,16 +3,16 @@ const ui = new Ui;
 
 const form = document.querySelector('form');
 const input = document.querySelector('#input');
-document.addEventListener('DOMContentloded',getWeather());
 
-  function getWeather() {
+
+  
     weather.getWeather('dhaka')
   .then(data =>{
     ui.paint(data)
   })
   .catch(err =>console.log(err)
   );
-  }
+  
 
   form.addEventListener('submit',e =>{
     e.preventDefault();
