5ed65650a91c5d7e265f416b0a51f09b1a97f31e
foysalkazi11 <foysalkazi11@gmail.com>
2020-05-16 21:21:37 +0600

    update local storage

--- script/app.js
+++ script/app.js
@@ -4,26 +4,23 @@ const ui = new Ui;
 const form = document.querySelector('form');
 const input = document.querySelector('#input');
 
-
+  // weather.getWeather('dhaka')
+  // .then(data =>{
+  //   ui.paint(data)
+  // })
+  // .catch(err =>console.log(err)
+  // );
   
-    weather.getWeather('dhaka')
-  .then(data =>{
-    ui.paint(data)
-  })
-  .catch(err =>console.log(err)
-  );
-  
-
   form.addEventListener('submit',e =>{
     e.preventDefault();
     let value = e.target.location.value;
       if (value !== '') {
-        
+        localStorage.setItem('place',value);
         weather.getWeather(value)
         .then(data=>{
           console.log(data);
           if (data.cod === '404') {
-            ui.showAlert('City is not found','alert alert-danger m-0')
+            ui.showAlert('City name is not found','alert alert-danger m-0')
           }else{
             ui.paint(data)
           }
@@ -37,7 +34,11 @@ const input = document.querySelector('#input');
     
   });
 
-
+if (localStorage.getItem('place')) {
+  weather.getWeather(localStorage.getItem('place'))
+  .then(data =>ui.paint(data))
+  .catch(err =>console.log(err));
+}
 
 
 
